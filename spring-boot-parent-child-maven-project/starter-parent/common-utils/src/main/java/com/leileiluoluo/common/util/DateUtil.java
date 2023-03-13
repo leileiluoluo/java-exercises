9ff29e7ddff37718a23f4125dc6c0c89e534bcee
Larry Yan <Lei.Lei.Yan@kyndryl.com>
2023-03-13 16:12:29 +0800

    spring boot parent child maven project

--- spring-boot-parent-child-maven-project/starter-parent/common-utils/src/main/java/com/leileiluoluo/common/util/DateUtil.java
+++ spring-boot-parent-child-maven-project/starter-parent/common-utils/src/main/java/com/leileiluoluo/common/util/DateUtil.java
@@ -4,6 +4,7 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
+
     public static String getCurrentTimeStr() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return ZonedDateTime.now().format(formatter);
