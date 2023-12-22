7d7d08da5ff66e7d08e529960494ef505a146b77
LeiLei Yan <olzhy@qq.com>
2023-12-22 14:38:44 +0800

    rest assured demo

---
+++ rest-assured-demo/src/test/java/com/example/tests/GitHubBranchAPITest.java
@@ -0,0 +1,29 @@
+package com.example.tests;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasItem;
+
+public class GitHubBranchAPITest {
+
+    @Test
+    public void listBranches() {
+        baseURI = "https://api.github.com/repos/olzhy/java-exercises";
+
+        given().accept(ContentType.JSON)
+                .header("Authorization", "Bearer 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")
+                .header("X-GitHub-Api-Version", "2022-11-28")
+                .queryParam("page", 1)
+                .queryParam("per_page", 10)
+                .when()
+                .get("/branches")
+                .then()
+                .statusCode(200)
+                .body("$", hasItem(hasEntry("name", "main")));
+    }
+
+}
