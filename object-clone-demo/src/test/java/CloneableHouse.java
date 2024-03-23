b4d9b5315832f5cd8e99ff7b7e58794d51dd515b
LeiLei Yan <olzhy@qq.com>
2024-03-23 17:32:57 +0800

    object clone demo

--- object-clone-demo/src/test/java/CloneableHouse.java
+++ object-clone-demo/src/test/java/CloneableHouse.java
@@ -1,18 +1,18 @@
-public class House implements Cloneable {
+public class CloneableHouse implements Cloneable {
     private String name;
     private Integer size;
     private Refrigerator refrigerator;
 
-    public House(String name, Integer size, Refrigerator refrigerator) {
+    public CloneableHouse(String name, Integer size, Refrigerator refrigerator) {
         this.name = name;
         this.size = size;
         this.refrigerator = refrigerator;
     }
 
     @Override
-    public House clone() {
+    public CloneableHouse clone() {
         try {
-            House house = (House) super.clone();
+            CloneableHouse house = (CloneableHouse) super.clone();
             house.refrigerator = house.refrigerator.clone();
             return house;
         } catch (CloneNotSupportedException e) {
@@ -60,25 +60,25 @@ public class House implements Cloneable {
 
     public static void main(String[] args) {
         Refrigerator refrigerator = new Refrigerator("Larry's Refrigerator", new Fruit("Apple"));
-        House house1 = new House("Larry's House", 100, refrigerator);
+        CloneableHouse house1 = new CloneableHouse("Larry's House", 100, refrigerator);
 
-        House house2 = house1.clone();
+        CloneableHouse house2 = house1.clone();
         house2.name = "Jacky's House";
         house2.size = 99;
         house2.refrigerator.name = "Jacky's Refrigerator";
         house2.refrigerator.fruit.name = "Banana";
 
-        System.out.println(house1); // House@6d311334
+        System.out.println(house1); // CloneableHouse@6d311334
         System.out.println(house1.name); // Larry's House
         System.out.println(house1.size); // 100
-        System.out.println(house1.refrigerator); // House$Refrigerator@682a0b20
+        System.out.println(house1.refrigerator); // CloneableHouse$Refrigerator@682a0b20
         System.out.println(house1.refrigerator.name); // Larry's Refrigerator
         System.out.println(house1.refrigerator.fruit.name); // Apple
 
-        System.out.println(house2); // House@3d075dc0
+        System.out.println(house2); // CloneableHouse@3d075dc0
         System.out.println(house2.name); // Jacky's House
         System.out.println(house2.size); // 99
-        System.out.println(house2.refrigerator); // House$Refrigerator@214c265e
+        System.out.println(house2.refrigerator); // CloneableHouse$Refrigerator@214c265e
         System.out.println(house2.refrigerator.name); // Jacky's Refrigerator
         System.out.println(house2.refrigerator.fruit.name); // Banana
     }
