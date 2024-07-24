57985789f7bbc311e6222404cede432885e9da09
Jane <Jane@larrys-MacBook-Pro.local>
2024-07-24 19:34:19 +0800

    java 8 new features demo

--- java-8-new-features-demo/src/main/java/StreamAPITest.java
+++ java-8-new-features-demo/src/main/java/StreamAPITest.java
@@ -1,8 +1,16 @@
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class StreamAPITest {
 
     public static void main(String[] args) {
+        // 生成一个 [1, 2, ..., 100] 的数组，然后对每个元素求平方后进行求和
+        long sum = IntStream.rangeClosed(0, 100)
+                .mapToLong(num -> (long) num * num)
+                .sum();
+        System.out.println(sum);
+
+        // 对 List 进行过滤、映射、排序后进行打印
         List<String> languages = List.of("java", "golang", "python", "php", "javascript");
         languages.stream()
                 .filter(lang -> lang.length() < 8)
