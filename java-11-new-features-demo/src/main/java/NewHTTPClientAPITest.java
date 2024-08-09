79ffa212bf62af32b6c0a7190c7536147d0299e8
Jane <Jane@larrys-MacBook-Pro.local>
2024-08-09 10:44:04 +0800

    java 11 new features demo

---
+++ java-11-new-features-demo/src/main/java/NewHTTPClientAPITest.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+
+public class NewHTTPClientAPITest {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // 构建 HttpClient 对象
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofMinutes(1))
+                .build();
+
+        // 构建 HttpRequest 对象
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://leileiluoluo.com"))
+                .GET()
+                .build();
+
+        // 同步请求
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+        // 异步请求
+        CompletableFuture<HttpResponse<String>> futureResponse = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        futureResponse.thenApply(HttpResponse::body) // 获取响应体
+                .thenAccept(System.out::println) // 打印响应体
+                .join(); // 等待所有操作完成
+    }
+
+}
