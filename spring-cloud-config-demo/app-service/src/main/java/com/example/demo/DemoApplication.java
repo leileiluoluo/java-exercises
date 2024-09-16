9c9d000a79da750e9847a381a37ba24e778dab4c
Jane <Jane@larrys-MacBook-Pro.local>
2024-09-16 19:35:36 +0800

    spring cloud config demo

--- spring-cloud-config-demo/app-service/src/main/java/com/example/demo/DemoApplication.java
+++ spring-cloud-config-demo/app-service/src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
 @RestController
 @EnableDiscoveryClient
 @SpringBootApplication
